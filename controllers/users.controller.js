a421014e5f0db4d95b2ef1584e363608cb5be258
Julia-Protasevich <protasevich.julia@gmail.com>
2020-05-28 21:02:33 +0300

    rewrite the whole login and remove dummy route

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,10 +1,6 @@
 import {User} from '../models/user.model.js';
 
 export const UsersController = {
-  async test(req, res) {
-    res.send('Greetings from the Test controller!');
-  },
-
   async login(ctx, next){
     await passport.authenticate('local', function (err, user) {
       if (user == false) {
@@ -26,17 +22,11 @@ export const UsersController = {
     try{
         const user = new User({ displayName: req.body.name, email: req.body.email, password: req.body.password});
 
-        await user.save(function(err) {
-          return err ? 
-            next(err)
-            : req.logIn(user, function(err) {
-              return err ? 
-                next(err)
-                : res.redirect('/private');
-            });
-        });
-    }catch(e){
-        next(e);
+        ctx.body = await User.create(user);
+        
+    }catch(err){
+      ctx.status = 400;
+      ctx.body = err;
     }
     
   }
