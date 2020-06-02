8c5dd919a53180ea5850832fa67296357a6bb3c1
Julia-Protasevich <protasevich.julia@gmail.com>
2020-06-02 19:33:58 +0300

    implement cors handler

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ import {usersRouter} from './routes/users.route.js';
 import databaseConnect from './database.js';
 import passport from './middleware/passport.js';
 import startServer from './server.js';
+import corsSetup from './middleware/cors.js';
 
 
 // initialize our express app
@@ -16,9 +17,11 @@ databaseConnect();
 
 app.use(passport.initialize()); 
 
+
 app.use('/books', booksRouter);
 app.use('/user', usersRouter);
 
+corsSetup(app);
 startServer(app);
 
 export default app;
